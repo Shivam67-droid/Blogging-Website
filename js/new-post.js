b001edfcd5b1f2899705eb343f5d138ca3bf164e
Shivam67-droid <singhshivam67899876@gmail.com>
2022-09-14 14:52:02 +0530

    first commit

---
+++ js/new-post.js
@@ -0,0 +1,25 @@
+/**
+ * TODO: Finish submitNewPost function to submit form data to the API
+ */
+
+const API_URL = 'https://blogging-appp.herokuapp.com/api/posts';
+
+const submitNewPost = () => {
+  // HINT: Use FormData to store data to send over
+  // HINT: Redirect the user to home page after successful submission
+  const title = document.getElementById('form-post-title').value;
+  const content = document.getElementById('form-post-content').value;
+  const input = document.getElementById('form-post-image');
+
+  let data = new FormData();
+  data.append('post-image', input.files[0]);
+  data.append('title', title);
+  data.append('content', content);
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: data,
+  }).then(() => {
+    window.location.href = 'index.html';
+  });
+};
